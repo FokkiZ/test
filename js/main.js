75fad55c317b3934829ed1f1e2b1bb1fd9e578b3
Ivan <fokkigame@gmail.com>
2023-01-05 12:52:49 +0200

    film

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const numberOfFilms = +prompt('kiko');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('yakui film'),
+      b = prompt('ocinka'),
+      c = prompt('yakui film'),
+      d = prompt('ocinka');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
